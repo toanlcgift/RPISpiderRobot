80d46e0d80482a2642b8f25f527c7cd14a5a29f1
Nguyễn Đức Toàn <toanlcgift@gmail.com>
2018-05-05 19:59:13 +0700

    update

--- LiveStreamServer/LiveStreamServer/Program.cs
+++ LiveStreamServer/LiveStreamServer/Program.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using SamplesCore;
+using WiringPi;
 
 namespace LiveStreamServer
 {
@@ -31,9 +33,24 @@ namespace LiveStreamServer
             //new SVMSample();
             //new VideoWriterSample();
             //new VideoCaptureSample();
-
-            sample.Run();
+            TestWiringPi();
+            //sample.Run();
             BuildWebHost(args).Run();
+        }
+
+        public static void TestWiringPi()
+        {
+            int ret = Init.WiringPiSetupGpio();
+            if (ret == -1)
+            {
+                Console.WriteLine("Init failed: {0}", ret);
+                return;
+            }
+            for (; ; )
+            {
+                WiringPi.GPIO.digitalWrite(0, 1);
+                Thread.Sleep(1000);
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
