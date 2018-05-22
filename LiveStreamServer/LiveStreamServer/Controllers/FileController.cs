0fb06cfdc04af335f5aee809819ccbb11f5038c9
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2018-05-22 23:57:24 +0700

    add file controller

---
+++ LiveStreamServer/LiveStreamServer/Controllers/FileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveStreamServer.Controllers
+{
+    [Route("api/File")]
+    public class FileController : Controller
+    {
+        [HttpGet("download.mp4")]
+        public FileResult DownloadFile()
+        {
+            var content = new FileStream(SamplesCore.FilePath.Movie.Bach, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var response = File(content, "application/octet-stream");//FileStreamResult
+            return response;
+        }
+    }
+}
